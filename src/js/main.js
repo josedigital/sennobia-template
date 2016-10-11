43e41b791ea82c62bfeb5fff8d05af596ba43077
alex rodriguez <alex.rodriguez@austintexas.gov>
2016-10-11 10:43:32 -0500

    update content

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,13 @@
-// put your code here
-// remember, you can use modules with browserify
+$(document).ready(function() {
+
+//add some smooth for scroll
+  $('.top-nav ul li a').on('click', function() {
+    $(window).stop(true).scrollTo(this.hash, {duration:1000, interrupt:true});
+    return false;
+  });
+  $('.js-button').on('click', function() {
+    $(window).stop(true).scrollTo(this.hash, {duration:1000, interrupt:true});
+    return false;
+  });
+
+});
